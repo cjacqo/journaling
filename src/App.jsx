31fbf27b5a762bf02f6d934d460d4ea67ac86571
cjacqo <76092910+cjacqo@users.noreply.github.com>
2023-12-19 15:55:08 -0800

    Started work on routes and parent states for user and token and entries

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,68 @@
 import './App.css'
+import { useEffect, useState } from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { LoginView, SignupView } from './components/views'
 
 function App() {
+  // Look for items in local storage
+  const storedUser = JSON.parse(localStorage.getItem('user'))
+  const storedToken = localStorage.getItem('token')
+  // State values for user and token if in local storage or set to null
+  const [user, setUser] = useState(storedUser ? storedUser : null)
+  const [token, setToken] = useState(storedToken ? storedToken : null)
+  // State values for user entries
+  const [entries, setEntries] = useState([])
 
+  // Fetch entries for the user
+  useEffect(() => {
+    if (!token) return 
+    fetchUserEntries()
+  }, [token])
+
+  const fetchUserEntries = () => {
+    fetch('https://journaling-api-7a082617967a.herokuapp.com/entries', {
+      headers: { Authorization: `Bearer ${token}` }
+    })
+      .then(res => res.json())
+      .then(data => {
+        setEntries(data)
+      })
+      .catch(err => console.error(err))
+  }
+  
   return (
-    <>
-      <LoginView onLoggedIn={() => console.log('logged in')} />
-      <SignupView onLoggedIn={() => console.log('signed up')} />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path='/signup'
+          element={
+            <>
+              {
+                user ? (
+                  <Navigate to='/' />
+                ) : (
+                  <SignupView />
+                )
+              }
+            </>
+          }
+        />
+        <Route
+          path='/login'
+          element={
+            <>
+              {
+                user ? (
+                  <Navigate to='/' />
+                ) : (
+                  <LoginView onLoggedIn={(user, token) => { setUser(user); setToken(token); }} />
+                )
+              }
+            </>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
