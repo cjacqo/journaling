4756800ac57746d5b52e1a49d4e67e9d5c5ec12e
cjacqo <76092910+cjacqo@users.noreply.github.com>
2023-12-20 14:47:29 -0800

    Days of week are now on calendar

--- src/components/calendar/Calendar.jsx
+++ src/components/calendar/Calendar.jsx
@@ -1,14 +1,20 @@
 import { Cell } from './calendarComponents'
 
 const Calendar = () => {
+  const daysOfWeek = ['Sun', 'Mon', 'Tues', 'Wed', 'Thur', 'Fri', 'Sat']
+
   return (
     <div className='w-[400px] border-t border-l'>
       <div className='grid grid-cols-7 items-center justify-center text-center'>
+        {/* Calendar Header */}
         <Cell>{'<<'}</Cell>
         <Cell>{'<'}</Cell>
         <Cell className='col-span-3'>December 2023</Cell>
         <Cell>{'>'}</Cell>
         <Cell>{'>>'}</Cell>
+
+        {/* Loop over daysOfWeek to make cells */}
+        { daysOfWeek.map((day, i) => <Cell key={i} className='text-sm font-bold'>{day}</Cell>) }
       </div>
     </div>
   )
