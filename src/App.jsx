c73be612f5816d3f34e4f8fb8dc1072f4484b23b
cjacqo <76092910+cjacqo@users.noreply.github.com>
2023-12-20 13:11:00 -0800

    Disabled exhaustive-deps on App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import './App.css'
 import { useEffect, useState } from 'react'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
