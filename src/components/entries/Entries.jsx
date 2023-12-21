d48be0e612e4862f0f1462952c5ebd3e9e2a4005
cjacqo <76092910+cjacqo@users.noreply.github.com>
2023-12-21 14:19:14 -0800

    Entries list is complete

--- src/components/entries/Entries.jsx
+++ src/components/entries/Entries.jsx
@@ -1,30 +1,15 @@
 import PropTypes from 'prop-types'
-import { format } from 'date-fns'
 import { EntriesList } from './entriesComponents'
 
 const Entries = ({ entries, currentDate, selectedDay }) => {
-  const isCurrentAndSelectedDateSameAsEntry = (entryDate) => {
-    const currentMonthAndYear = format(currentDate, 'LLLL yyyy')
-    const selectedMonthAndYear = format(selectedDay, 'LLLL yyyy')
-    const selectedDayFormat = format(selectedDay, 'LLLL dd yyyy')
-    const entryDateFormat = format(new Date(entryDate), 'LLLL dd yyyy')
-
-    return currentMonthAndYear === selectedMonthAndYear && selectedDayFormat === entryDateFormat
-  }
   return (
     <div>
       {/* Display a list of the entries */}
       <EntriesList
         entries={entries}
-        showEntries={isCurrentAndSelectedDateSameAsEntry} />
+        currentDate={currentDate}
+        selectedDay={selectedDay} />
       
-      {/* {
-        entries.map(entry => {
-          if (isCurrentAndSelectedDateSameAsEntry(entry.CreatedAt)) {
-            return <Entry key={entry._id} entry={entry} />
-          }
-        })
-      } */}
     </div>
   )
   
--- src/components/entries/entriesComponents/EntriesList.jsx
+++ src/components/entries/entriesComponents/EntriesList.jsx
@@ -1,20 +1,47 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useState, useEffect } from 'react'
 import { format } from 'date-fns'
 import PropTypes from 'prop-types'
 
-const EntriesList = ({ entries, showEntries }) => {
+const EntriesList = ({ entries, currentDate, selectedDay }) => {
+  const [shownEntries, setShownEntries] = useState([])
+
+  useEffect(() => {
+    getEntriesForSelectedDay()
+  }, [entries, currentDate, selectedDay])
+
+  const currentMonthAndYear = format(currentDate, 'LLLL yyyy')
+  const selectedMonthAndYear = format(selectedDay, 'LLLL yyyy')
+  const selectedDayFormat = format(selectedDay, 'LLLL dd yyyy')
+
+  const isCurrentAndSelectedDateSameAsEntry = (entryDate) => {
+    const entryDateFormat = format(new Date(entryDate), 'LLLL dd yyyy')
+    return currentMonthAndYear === selectedMonthAndYear && selectedDayFormat === entryDateFormat
+  }
+  
+  const getEntriesForSelectedDay = () => {
+    setShownEntries(entries.filter(entry => selectedDayFormat === format(new Date(entry.CreatedAt), 'LLLL dd yyyy')))
+  }
   return (
     <div className='container mx-auto flex flex-col gap-4 mt-3'>
       {
-        entries.map(entry => {
-          if (showEntries(entry.CreatedAt)) {
-            return (
-              <div key={entry._id} className='flex justify-between border px-2 py-3 w-100'>
-                <h3>{entry.Title}</h3>
-                <p>{format(entry.CreatedAt, 'LLL dd yyyy')}</p>
-              </div>
-            )
-          }
-        })
+        entries.length !== 0 && shownEntries.length === 0 ? (
+          <div>
+            <h3>No entries</h3>
+          </div>
+        ) : (
+          shownEntries.map(entry => {
+            if (isCurrentAndSelectedDateSameAsEntry(entry.CreatedAt)) {
+              return (
+                <div key={entry._id} className='flex justify-between border px-2 py-3 w-100'>
+                  <h3>{entry.Title}</h3>
+                  <p>{format(entry.CreatedAt, 'LLL dd yyyy')}</p>
+                </div>
+              )
+            }
+            
+          })
+        )
       }
     </div>
   )
@@ -22,7 +49,8 @@ const EntriesList = ({ entries, showEntries }) => {
 
 EntriesList.propTypes = {
   entries: PropTypes.array.isRequired,
-  showEntries: PropTypes.func.isRequired
+  currentDate: PropTypes.object.isRequired,
+  selectedDay: PropTypes.object.isRequired
 }
 
 export default EntriesList
